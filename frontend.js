13e5b1161d9985ccd52b422d6525799f1656e42a
abid000007 <abid.raheem@blocksdev.com>
2023-08-30 17:23:02 +0500

    test

--- frontend.js
+++ frontend.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello, YOU ARE HACKED.');
+    res.send('Hello, YOU ARE not hacked.');
 });
 
 app.listen(port, () => {
