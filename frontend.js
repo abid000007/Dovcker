4a68ea508b3517cfa8ed8164818bb44c994fd50c
abid000007 <abid.raheem@blocksdev.com>
2023-08-30 15:19:23 +0500

    ting

---
+++ frontend.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to log the IP address
+app.use((req, res, next) => {
+    const clientIp = req.ip;
+    console.log(`Visitor IP: ${clientIp}`);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, your IP has been logged.');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
